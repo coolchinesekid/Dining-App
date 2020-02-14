70e7cf88e316bb6b007b35512ed21b207285a391
Dylan MacFarlane <dylan@macfarlanehome.com>
2020-02-14 13:45:51 -0800

    Initial Commit

---
+++ Formal Dinner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Formal Dinner
+//
+//  Created by Dylan MacFarlane on 2/14/20.
+//  Copyright © 2020 Dylan MacFarlane. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
